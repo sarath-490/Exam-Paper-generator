41642e18a4e054830e94958e22614bb97b939b79
Sarath Sai Redrouthu <sarath2005sai0505@gmail.com>
2025-11-11 22:21:29 +0530

    Fix import path for uvicorn run command

--- backend/app/main.py
+++ backend/app/main.py
@@ -55,5 +55,8 @@ app.include_router(admin.router)
 app.include_router(teacher.router)
 
 if __name__ == "__main__":
+    import os
+    import uvicorn
+
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("backend.app.main:app", host="0.0.0.0", port=port)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
